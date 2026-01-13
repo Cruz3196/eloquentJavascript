bbc1cac36763fcac2c76adaec217d8ce4e1a3566
Jose Cruz <159391963+Cruz3196@users.noreply.github.com>
2026-01-13 01:06:14 -0800

    fizzBuzz challenge completed using nested loops

---
+++ fizzBuzz.js
@@ -0,0 +1,33 @@
+/**
+ * This is the fizzbuzz game
+ *
+ * create an arr
+ *
+ * for loop that will iterate from 1 to 100
+ *    use the modular operator %
+ *      print "fizz" that divisible by 3
+ *      print "buzz" that are divisible by 5
+ *      print "fizzbuzz" that are divisible by 3 & 5
+ *    increment; add the arr
+ * print from 1 to 100
+ *
+ */
+function fizzBuzz() {
+  let zeroToHundred = [];
+  function mod() {
+    for (let counter = 1; counter <= 100; counter++) {
+      if (counter % 3 == 0 && counter % 5 == 0) {
+        console.log("FizzBuzz");
+      } else if (counter % 3 == 0) {
+        console.log("Fizz");
+      } else if (counter % 5 == 0) {
+        console.log("Buzz");
+      } else {
+        console.log(counter);
+      }
+    }
+    return zeroToHundred;
+  }
+  console.log(mod().join(" "));
+}
+fizzBuzz();
